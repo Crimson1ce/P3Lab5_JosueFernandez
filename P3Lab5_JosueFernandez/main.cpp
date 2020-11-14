e473702b21325ad6de5ef34e526b32abfe59660f
Josue <davyd3501@gmail.com>
2020-11-13 18:28:15 -0600

    Funcionalidad terminada para las operaciones. No probada aun.

--- P3Lab5_JosueFernandez/main.cpp
+++ P3Lab5_JosueFernandez/main.cpp
@@ -35,7 +35,7 @@ void liberarMatriz(int**& matriz, int nFilas);
 //Imprime la matriz
 void imprimirMatriz(int ** matriz, int nFilas, int nCol);
 //Multiplica dos matrices (si es posible) y las añade al vector de matrices
-bool multiplicarMatrices(int** m1, int nFilas, int nCol, int** m2, int nnFilas, int nnCol);
+int** multiplicarMatrices(int** m1, int nFilas, int nCol, int** m2, int nnFilas, int nnCol);
 void operar(string operaciones);
 
 int main(int argc, char** argv) {
@@ -43,7 +43,7 @@ int main(int argc, char** argv) {
     srand(time(0));
 
     int cantidadActual = 0;
-    
+
     int opcion = 0;
     string operaciones = "";
     do {
@@ -79,19 +79,25 @@ int main(int argc, char** argv) {
                 break;
             case 3:
                 cantidadActual = vectorSize;
-                
+
                 operaciones = "";
                 cout << "Ingrese las operaciones a realizar: ";
                 cin >> operaciones;
 
                 operar(operaciones);
+                for (int i = cantidadActual; i < vectorSize; i++) {
+                    int** matriz = lista.at(i);
+                    liberarMatriz(matriz, filas.at(i));
+                }
 
+                lista.erase(lista.begin() + cantidadActual, lista.end());
+                lista.shrink_to_fit();
 
-                //                cout << "Multiplicando 0 y 1 " << endl;
-                //                
-                //                if(multiplicarMatrices(lista.at(0), filas.at(0), columnas.at(0), lista.at(1), filas.at(1), columnas.at(1))){
-                //                    imprimirMatriz(lista.at(vectorSize-1),filas.at(vectorSize-1), columnas.at(vectorSize-1));
-                //                }
+                filas.erase(filas.begin() + cantidadActual, filas.end());
+                filas.shrink_to_fit();
+
+                columnas.erase(columnas.begin() + cantidadActual, columnas.end());
+                columnas.shrink_to_fit();
 
                 break;
             case 4:
@@ -198,10 +204,10 @@ void imprimirMatriz(int ** matriz, int nFilas, int nCol) {
     }
 }
 
-bool multiplicarMatrices(int** m1, int nFilas, int nCol,
+int** multiplicarMatrices(int** m1, int nFilas, int nCol,
         int** m2, int nnFilas, int nnCol) {
     if (nCol != nnFilas) {
-        return false;
+        return NULL;
     }
     int** nueva = new int*[nFilas];
     for (int i = 0; i < nFilas; i++) {
@@ -215,72 +221,86 @@ bool multiplicarMatrices(int** m1, int nFilas, int nCol,
     }
 
     //Agregamos la nueva matriz a la lista
-    lista.push_back(nueva);
-    filas.push_back(nFilas);
-    columnas.push_back(nnCol);
-    vectorSize++;
+    //    lista.push_back(nueva);
+    //    filas.push_back(nFilas);
+    //    columnas.push_back(nnCol);
+    //    vectorSize++;
 
-    return true;
+    return nueva;
 }
 
-int contar(string cadena, char caracter){
+int contar(string cadena, char caracter) {
     int contador = 0;
-    
-    for(int i=0; i<cadena.length(); i++){
-        if(cadena[i] == caracter){
+
+    for (int i = 0; i < cadena.length(); i++) {
+        if (cadena[i] == caracter) {
             contador++;
         }
     }
-    
+
     return contador;
 }
 
-bool sumarMatrices(int** m1, int nFilas, int nCol,
+int** sumarMatrices(int** m1, int nFilas, int nCol,
         int** m2, int nnFilas, int nnCol) {
-    
-    if(nFilas != nnFilas || nCol != nnCol){
-        false;
+
+    if (nFilas != nnFilas || nCol != nnCol) {
+        return NULL;
     }
-    
+
     int** nueva = new int*[nFilas];
     for (int i = 0; i < nFilas; i++) {
         nueva[i] = new int[nCol];
         for (int j = 0; j < nCol; j++) {
-            nueva[i][j] =  (m1[i][j] + m2[i][j]);
+            nueva[i][j] = (m1[i][j] + m2[i][j]);
         }
     }
 
     //Agregamos la nueva matriz a la lista
-    lista.push_back(nueva);
-    filas.push_back(nFilas);
-    columnas.push_back(nnCol);
-    vectorSize++;
-    
-    return true;
+    //    lista.push_back(nueva);
+    //    filas.push_back(nFilas);
+    //    columnas.push_back(nnCol);
+    //    vectorSize++;
+
+    return nueva;
 }
 
-bool restarMatrices(int** m1, int nFilas, int nCol,
+int** restarMatrices(int** m1, int nFilas, int nCol,
         int** m2, int nnFilas, int nnCol) {
-    
-    if(nFilas != nnFilas || nCol != nnCol){
-        false;
+
+    if (nFilas != nnFilas || nCol != nnCol) {
+        return NULL;
     }
-    
+
     int** nueva = new int*[nFilas];
     for (int i = 0; i < nFilas; i++) {
         nueva[i] = new int[nCol];
         for (int j = 0; j < nCol; j++) {
-            nueva[i][j] =  (m1[i][j] - m2[i][j]);
+            nueva[i][j] = (m1[i][j] - m2[i][j]);
         }
     }
 
     //Agregamos la nueva matriz a la lista
-    lista.push_back(nueva);
-    filas.push_back(nFilas);
-    columnas.push_back(nnCol);
-    vectorSize++;
-    
-    return true;
+    //    lista.push_back(nueva);
+    //    filas.push_back(nFilas);
+    //    columnas.push_back(nnCol);
+    //    vectorSize++;
+
+    return nueva;
+}
+
+int** multiplicacionRecursiva(vector<int> numeros, int sizeNum, int i) {
+
+    if (sizeNum < 2 || i > sizeNum - 2) {
+        cout << "ERRRRRRRRRRRRRRRRRRRRRRRRORRRRRRRRRRRRRRRRRRR " << sizeNum << endl;
+        return NULL;
+    } else if (i == sizeNum - 2) {
+        return multiplicarMatrices(lista.at(numeros.at(sizeNum - 2)), filas.at(numeros.at(sizeNum - 2)), columnas.at(numeros.at(sizeNum - 2)),
+                lista.at(numeros.at(sizeNum - 1)), filas.at(numeros.at(sizeNum - 1)), columnas.at(numeros.at(sizeNum - 1)));
+    } else {
+        return multiplicarMatrices(lista.at(numeros.at(i)), filas.at(numeros.at(i)), columnas.at(numeros.at(i)),
+                multiplicacionRecursiva(numeros, sizeNum, i + 1), filas.at(numeros.at(i + 1)), columnas.at(numeros.at(sizeNum - 1)));
+    }
 }
 
 void operar(string operaciones) {
@@ -292,8 +312,8 @@ void operar(string operaciones) {
     int sizeTer = 0;
     vector <string> terminos;
 
-    
-    
+
+
     //string auxiliar;
     int ultimo = 0;
     for (int i = 0; i < operaciones.length(); i++) {
@@ -310,20 +330,69 @@ void operar(string operaciones) {
     terminos.push_back(operaciones.substr(ultimo));
     sizeTer++;
 
-    
+
     for (int i = 0; i < sizeTer; i++) {
-        
-        int cantidad = contar(terminos.at(i),'*');
-        
-        string parsed;
-        stringstream ss;
-        
-        if(cantidad > 0){
-            for (int j = 0; j < size; j++) {
+
+        string termino = terminos.at(i);
+        int cantidad = contar(termino, '*');
+
+
+        //Si hay alguna multiplicacion
+        if (cantidad > 0) {
+            int sizeNum = 0;
+            vector <int> numeros;
+            stringstream ss(termino);
+            string item;
+
+            while (getline(ss, item, '*')) {
+                int num = stoi(item, nullptr, 10);
+                numeros.push_back(num);
+                sizeNum++;
             }
+
+            int ** producto = multiplicacionRecursiva(numeros, sizeNum, 0);
+
+            //Agregamos la nueva matriz a la lista
+            lista.push_back(producto);
+            filas.push_back(filas.at(numeros.at(0)));
+            columnas.push_back(columnas.at(numeros.at(sizeNum - 1)));
+            vectorSize++;
+
+            terminos[i] = to_string(vectorSize - 1);
+
         }
     }
 
+    //Sumas y restas
+    int ** matriz = lista.at(stoi(terminos[0], nullptr, 10));
+    int fil = filas.at(stoi(terminos[0], nullptr, 10));
+    ;
+    int col = columnas.at(stoi(terminos[0], nullptr, 10));
+    ;
+
+    for (int i = 0; i < sizeOp; i++) {
+
+        int ** auxiliar;
+        if (operadores[i] == '+') {
+            auxiliar = sumarMatrices(matriz, fil, col,
+                    lista.at(stoi(terminos[i + 1], nullptr, 10)), 
+                    filas.at(stoi(terminos[i + 1], nullptr, 10)), 
+                    columnas[stoi(terminos[i + 1], nullptr, 10)]);
+        } else {
+            auxiliar = restarMatrices(matriz, fil, col,
+                    lista.at(stoi(terminos[i + 1], nullptr, 10)), 
+                    filas.at(stoi(terminos[i + 1], nullptr, 10)), 
+                    columnas[stoi(terminos[i + 1], nullptr, 10)]);
+        }
+        if (auxiliar == NULL) {
+            cout << "No se pudo realizar la operacion" << endl;
+            return;
+        }
+        liberarMatriz(matriz, fil);
+        matriz = auxiliar;
+    }
+
+    imprimirMatriz(matriz, fil, col);
 
 }
 
